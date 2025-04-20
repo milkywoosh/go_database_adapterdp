bf7f3e29c17c89effc1899f59d2ff9a6b252233e
sigma.lukman <auliya.lukman@sigma.co.id>
2025-04-20 13:28:53 +0700

    add note

--- db/tx_create_user.go
+++ db/tx_create_user.go
@@ -4,7 +4,7 @@ import "context"
 
 type CreateUserTxParams struct {
 	CreateUserParams
-	AfterCreate func(user Users) error
+	AfterCreate func(user Users) error // note: diisi function APAPUN yg penting passing argument tipe Users dan return Error!
 }
 
 func (store *SQLStore) CreateUserTx(ctx context.Context, arg CreateUserTxParams) (CreateUserTxResult, error) {
@@ -12,7 +12,8 @@ func (store *SQLStore) CreateUserTx(ctx context.Context, arg CreateUserTxParams)
 
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
-
+		// means: AfterCreate is a function field (callback) that takes a Users object and returns an error.
+		// It's a callback that's run after the user has been created in the database, inside the transaction.
 		result.Users, err = q.CreateUser(ctx, arg.CreateUserParams)
 		if err != nil {
 			return err
