bbc37cfd3b7819f654abb10303cb9344b40ebce1
sigma.lukman <auliya.lukman@sigma.co.id>
2025-05-03 23:36:00 +0700

    add new signature

--- db/store.go
+++ db/store.go
@@ -5,18 +5,22 @@ import (
 	"database/sql"
 )
 
+// note misalnya tidak dalam transaksi, apakah akan terjadi update sebagian??? iyaa
+// note jika akan melakukan UPDATE, INSERT disertai logic harus dalam *SQLStore execTx() function!!!
+
 type Store interface {
 	Querier
 	CreateUserTx(ctx context.Context, arg CreateUserTxParams) (CreateUserTxResult, error)
 	PurchaseBookTx(ctx context.Context, arg CreatePurchaseBookTxParams) (CreatePurchaseBookTxResult, error)
+	EditListBookTx(ctx context.Context, arg EditBookToPurchaseParams) (int64, error)
 }
 
 type SQLStore struct {
-	connPool *sql.DB // *godror.Conn()
+	connPool *sql.DB // *godror.Conn() atau pgx.Conn() depend definisi dbtype
 	*Queries
 }
 
-func NewStore(connPool *sql.DB, dbtype_arg string) *SQLStore{
+func NewStore(connPool *sql.DB, dbtype_arg string) *SQLStore {
 	return &SQLStore{
 		connPool: connPool,
 		Queries:  New(connPool, dbtype_arg),
