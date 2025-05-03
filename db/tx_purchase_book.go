8731540652a48b869f5a94f962ae3738da18fdeb
sigma.lukman <auliya.lukman@sigma.co.id>
2025-05-03 23:35:51 +0700

    edit tx purchase

--- db/tx_purchase_book.go
+++ db/tx_purchase_book.go
@@ -1,1 +1,18 @@
 package db
+
+import "context"
+
+type CreatePurchaseBookTxParams struct{}
+type CreatePurchaseBookTxResult struct{}
+
+func (store *SQLStore) PurchaseBookTx(ctx context.Context, arg CreatePurchaseBookTxParams) (CreatePurchaseBookTxResult, error) {
+	// var result_purchase_history PurchaseHistory
+	// var err error
+	// err = store.execTx(ctx, func(q *Queries) error {
+	// 	result_purchase_history, err = q.CreatePurchaseHistory(ctx, CreatePurchaseHistoryParams{})
+
+	// 	return err
+	// })
+
+	return CreatePurchaseBookTxResult{}, nil
+}
