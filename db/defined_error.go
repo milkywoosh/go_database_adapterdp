74653d429fb06041df3db737da5015d5370e03e1
sigma.lukman <auliya.lukman@sigma.co.id>
2025-05-03 23:35:29 +0700

    error standardization

---
+++ db/defined_error.go
@@ -0,0 +1,10 @@
+package db
+
+var ErrStokBukuHabis error
+var ErrStokBukuKurang error
+var ErrJenisPesananDuplikat error
+var ErrIDBukuTidakTerdaftar error
+var ErrUpdateNolData error
+var ErrUpdateMultipleData error
+var ErrNegativeNumber error
+var ErrFieldTableNotExisted error
