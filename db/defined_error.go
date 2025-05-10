47dc17ff7884165b722c92443b9e983c4f8a6803
sigma.lukman <auliya.lukman@sigma.co.id>
2025-05-10 10:15:29 +0700

    add note

--- db/defined_error.go
+++ db/defined_error.go
@@ -1,60 +1,83 @@
 package db
 
-type ErrStokBukuHabis struct{ Msg string }
+// all structs implement Error() string signature. Owned by Error Interface
+// so i can set them ass Error return type
+
+type ErrStokBukuHabis struct {
+	Msg string
+}
 
 func (e ErrStokBukuHabis) Error() string {
 	return e.Msg
 }
 
-type ErrStokBukuKurang struct{ Msg string }
+type ErrStokBukuKurang struct {
+	Msg string
+}
 
 func (e ErrStokBukuKurang) Error() string {
 	return e.Msg
 }
 
-type ErrJenisPesananDuplikat struct{ Msg string }
+type ErrJenisPesananDuplikat struct {
+	Msg string
+}
 
 func (e ErrJenisPesananDuplikat) Error() string {
 	return e.Msg
 }
 
-type ErrIDBukuTidakTerdaftar struct{ Msg string }
+type ErrIDBukuTidakTerdaftar struct {
+	Msg string
+}
 
 func (e ErrIDBukuTidakTerdaftar) Error() string {
 	return e.Msg
 }
 
-type ErrUpdateNolData struct{ Msg string }
+type ErrUpdateNolData struct {
+	Msg string
+}
 
 func (e ErrUpdateNolData) Error() string {
 	return e.Msg
 }
 
-type ErrUpdateMultipleData struct{ Msg string }
+type ErrUpdateMultipleData struct {
+	Msg string
+}
 
 func (e ErrUpdateMultipleData) Error() string {
 	return e.Msg
 }
 
-type ErrNegativeNumber struct{ Msg string }
+type ErrNegativeNumber struct {
+	Msg string
+}
 
 func (e ErrNegativeNumber) Error() string {
 	return e.Msg
 }
 
-type ErrFieldTableNotExisted struct{ Msg string }
+type ErrFieldTableNotExisted struct {
+	Msg string
+}
 
 func (e ErrFieldTableNotExisted) Error() string {
 	return e.Msg
 }
 
-type ErrStatusNotAcceptable struct{ Msg string }
+type ErrStatusNotAcceptable struct {
+	Msg string
+}
 
 func (e ErrStatusNotAcceptable) Error() string {
 	return e.Msg
 }
 
-type ErrDBTypeNotImplemented struct{ Msg string }
+type ErrDBTypeNotImplemented struct {
+	Msg string
+}
 
 func (e ErrDBTypeNotImplemented) Error() string {
 	return e.Msg
