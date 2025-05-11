c41ddc89fdec82243dac08d64e50732c47e1989e
sigma.lukman <auliya.lukman@sigma.co.id>
2025-05-12 06:59:44 +0700

    init connection PG database

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
@@ -11,7 +11,9 @@ import (
 	_ "github.com/lib/pq"
 
 	dotenv "github.com/lpernett/godotenv"
+	"github.com/luke_design_pattern/api"
 	"github.com/luke_design_pattern/config"
+	"github.com/luke_design_pattern/db"
 )
 
 func main() {
@@ -21,10 +23,10 @@ func main() {
 		return
 	}
 
-	tbl_ora := os.Getenv("ORA_DB_NAME")
-	pws_ora := os.Getenv("ORA_DB_PASSWORD")
-	addr_ora := os.Getenv("ORA_DB_CONN_STRING")
-	libdir_ora := os.Getenv("ORA_DB_LIB_DIR")
+	// tbl_ora := os.Getenv("ORA_DB_NAME")
+	// pws_ora := os.Getenv("ORA_DB_PASSWORD")
+	// addr_ora := os.Getenv("ORA_DB_CONN_STRING")
+	// libdir_ora := os.Getenv("ORA_DB_LIB_DIR")
 
 	tbl_pg := os.Getenv("PG_DB_NAME")
 	host_pg := os.Getenv("PG_DB_HOST")
@@ -35,11 +37,11 @@ func main() {
 	ssl_pg := os.Getenv("PG_DB_SSL")
 	libdir_pg := os.Getenv("PG_DB_LIB_DIR")
 
-	new_ora_conn, err := config.NewCredential("ORACLE", "", "", tbl_ora, "", pws_ora, addr_ora, libdir_ora, "")
-	if err != nil {
-		log.Fatalf("ORA: %v ", err)
-		return
-	}
+	// new_ora_conn, err := config.NewCredential("ORACLE", "", "", tbl_ora, "", pws_ora, addr_ora, libdir_ora, "")
+	// if err != nil {
+	// 	log.Fatalf("ORA: %v ", err)
+	// 	return
+	// }
 	// POSTGRES_HOST=127.0.0.1
 	// POSTGRES_USERNAME=postgres
 	// POSTGRES_PASSWORD=postgres
@@ -53,22 +55,22 @@ func main() {
 		return
 	}
 
-	ora_credential, err := new_ora_conn.GetConnectionString()
-	if err != nil {
-		log.Fatalf("===> ora get conn string:  %v", err)
-		return
-	}
+	// ora_credential, err := new_ora_conn.GetConnectionString()
+	// if err != nil {
+	// 	log.Fatalf("===> ora get conn string:  %v", err)
+	// 	return
+	// }
 	pg_credential, err := new_pg_conn.GetConnectionString()
 	if err != nil {
 		log.Fatalf("===> pg get conn string:  %v", err)
 		return
 	}
 
-	db_ora, err := sql.Open("godror", ora_credential)
-	if err != nil {
-		log.Fatalf("sql.Open godror failed: %v", err)
-		return
-	}
+	// db_ora, err := sql.Open("godror", ora_credential)
+	// if err != nil {
+	// 	log.Fatalf("sql.Open godror failed: %v", err)
+	// 	return
+	// }
 
 	// note => use stdlib
 	// db_pg, err := sql.Open("postgres", pg_credential)
@@ -78,18 +80,27 @@ func main() {
 		return
 	}
 	db_pg := stdlib.OpenDB(*configpg)
-	defer db_ora.Close()
+
+	// defer db_ora.Close()
 	defer db_pg.Close()
 
-	if err := db_ora.Ping(); err != nil {
-		log.Fatalf("DB Ping ora failed: %v", err)
-		return
-	}
+	// if err := db_ora.Ping(); err != nil {
+	// 	log.Fatalf("DB Ping ora failed: %v", err)
+	// 	return
+	// }
 	if err := db_pg.Ping(); err != nil {
 		log.Fatalf("DB Ping pg failed: %v", err)
 		return
 	}
 
-	log.Printf("load main: %s", "success connect!")
+	pg_sql := db.NewStore(db_pg, "POSTGRES")
+	server_api, err := api.NewServer(new_pg_conn, pg_sql)
+	if err != nil {
+		log.Fatalf("api.NewServer failed: %v", err)
+		os.Exit(1)
+	}
+
+	// note: port apps and PORT to DB is different
+	server_api.Start(fmt.Sprintf(":%s", "8000"))
 
 }
