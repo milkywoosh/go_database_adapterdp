32b5d1b3eb0b5fd66ac0e0f1185d27ef66b67a98
sigma.lukman <auliya.lukman@sigma.co.id>
2025-05-30 04:34:47 +0700

    add log.Printf err check in execTx

--- internal/exec_tx.go
+++ internal/exec_tx.go
@@ -3,18 +3,21 @@ package internal
 import (
 	"context"
 	"fmt"
+	"log"
 )
 
 func (userstore *UserStore) execTx(ctx context.Context, fn func(*UserQueries) error) error {
 
 	tx, err := userstore.connPool.BeginTx(ctx, nil)
 	if err != nil {
+		log.Printf("log err user store 1: %v", err)
 		return err
 	}
 
 	q := NewUserQuery(tx, userstore.dbtype)
 	err = fn(q) // kinda callback
 	if err != nil {
+		log.Printf("log err user store 2: %v", err)
 		if rbErr := tx.Rollback(); rbErr != nil {
 			return fmt.Errorf("tx err: %w rb err: %w", err, rbErr)
 		}
@@ -28,12 +31,14 @@ func (purchasestore *PurchaseStore) execTx(ctx context.Context, fn func(*Purchas
 
 	tx, err := purchasestore.connPool.BeginTx(ctx, nil)
 	if err != nil {
+		log.Printf("log err purchase store 1: %v", err)
 		return err
 	}
 
 	q := NewPurchaseQueries(tx, purchasestore.dbtype)
 	err = fn(q) // kinda callback
 	if err != nil {
+		log.Printf("log err purchase store 2: %v", err)
 		if rbErr := tx.Rollback(); rbErr != nil {
 			return fmt.Errorf("tx err: %w rb err: %w", err, rbErr)
 		}
