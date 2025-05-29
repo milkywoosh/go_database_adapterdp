88fb4c260718909aecf93915813b2e6148e0606c
sigma.lukman <auliya.lukman@sigma.co.id>
2025-05-30 03:56:18 +0700

    move to prc models

--- internal/purchase_models.go
+++ internal/purchase_models.go
@@ -41,3 +41,49 @@ type EditBookToPurchaseParams struct {
 	PurchaseHistoryID int
 	PurchaseNumber    string
 }
+
+type CreatePurchaseBookParams struct {
+	// Date               *time.Time ==> auto generate from golang time.Date
+	BookID            int
+	PurchaseHistoryID int
+	Qty               int
+	TotalPrice        int
+}
+
+type CreatePurchaseHistoryResult struct {
+}
+
+type BookToPurchase struct {
+	BookID            int
+	Qty               int
+	PurchaseHistoryID int
+	CurrentStockQty   int
+}
+
+type ListBooksToPurchase []BookToPurchase
+
+type CreateBookToPurchaseParams struct {
+	BookID            int
+	PurchaseHistoryID int
+	Qty               int
+	TotalPrice        float64
+	PurchaseNumber    string
+}
+
+type CreatePurchaseHistoryParams struct {
+	// Date              *time.Time ==> auto generate from golang time.Date
+	CustomerID        int
+	TotalPricePayment float64
+	Status            string // pending or completed
+	PurchaseNumber    string // PRCBOOK_20250421_RANDOMCHAR
+}
+
+type PurchaseDatatable struct {
+	PurchaseNumber string     `json:"purchase_number"`
+	Qty            int        `json:"qty"`
+	BookTitle      string     `json:"book_title"`
+	TotalPrice     float64    `json:"total_price"`
+	EachPrice      float64    `json:"each_price"`
+	DateOfSale     *time.Time `json:"date_of_sale"`
+	Status         string     `json:"status"`
+}
